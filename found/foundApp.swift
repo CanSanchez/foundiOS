80d7d911001a493baf51202f60892982ecb074db
CanSanchez <asanchez24@my.bcit.ca>
2024-02-28 17:14:52 -0800

    Initial Commit

---
+++ found/foundApp.swift
@@ -0,0 +1,17 @@
+//
+//  foundApp.swift
+//  found
+//
+//  Created by Can Sanchez on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct foundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
