c1dcbaa51244a35c4d0d9c251fef6e50fec05acb
CanSanchez <asanchez24@my.bcit.ca>
2024-03-06 15:27:11 -0800

    initial commit

--- found/ContentView.swift
+++ found/ContentView.swift
@@ -8,17 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+    @State private var animateGradient = false
+    @State private var isStarted = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.primaryPurple, .secondaryPurple, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+                .onAppear {
+                    withAnimation(.linear(duration: 2.0).repeatForever(autoreverses: true)) {
+                        animateGradient.toggle()
+                    }
+                }
+            if !isStarted {
+                Button {
+                    isStarted.toggle()
+                } label: {
+                    HStack {
+                        Text("Get Started")
+                            .font(.headline)
+                    }
+                    .fixedSize()
+                    .padding()
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .tint(.primaryPurple)
+                .shadow(radius: 7, x: 1, y: 3)
+            } else {
+                VStack {
+                    Text("Tell us what happened?")
+                    Button {
+                        print("test")
+                    } label: {
+                        HStack {
+                            Text("I lost a pet")
+                                .font(.headline)
+                        }
+                        .fixedSize()
+                        .padding()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.capsule)
+                    .tint(.primaryPurple)
+                    .shadow(radius: 7, x: 1, y: 3)
+                    Button {
+                        print("test")
+                    } label: {
+                        HStack {
+                            Text("I found a pet")
+                                .font(.headline)
+                        }
+                        .fixedSize()
+                        .padding()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.capsule)
+                    .tint(.primaryPurple)
+                    .shadow(radius: 7, x: 1, y: 3)
+                }
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
